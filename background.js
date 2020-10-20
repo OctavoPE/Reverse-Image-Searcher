d888e13b6eff72b24d4738a3271e4fd1dcb5f0fc
OctavoPE <62905048+OctavoPE@users.noreply.github.com>
2020-10-19 21:25:27 -0600

    Add files via upload

---
+++ background.js
@@ -0,0 +1,24 @@
+//console.log("Extension loaded...");
+
+browser.menus.create({
+  id: "findImg",
+  title: "Search for this image",
+  contexts: ["image"]
+});
+
+browser.menus.onClicked.addListener(async function (info, tab){
+  if(info.menuItemId == "findImg"){
+    if (info.srcUrl){
+      let imgUrl = info.srcUrl;
+      let googlePrefix = "https://www.google.com/searchbyimage?image_url=";
+      let yandexPrefix = "https://yandex.com/images/search?source=collection&&url=";
+      let yandexSuffix = "&rpt=imageview";
+      let bingSuffix = "https://www.bing.com/images/searchbyimage?FORM=IRSBIQ&cbir=sbi&imgurl=";
+
+      let yandexTab = await browser.tabs.create({ 'active': false, 'url': yandexPrefix+imgUrl+yandexSuffix, 'index': tab.index+1 });
+      let bingTab = await browser.tabs.create({ 'active': false, 'url': bingSuffix+imgUrl, 'index': tab.index+1 });
+      let googleTab = await browser.tabs.create({ 'active': false, 'url': googlePrefix+imgUrl, 'index': tab.index+1 });
+      
+    }
+  }
+});
